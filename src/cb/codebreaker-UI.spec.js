9c7d1a1617d79799a5e619075c8e0a7e5132d8d0
Juan Gabardini <jgabardini@computer.org>
2021-11-16 14:47:35 -0300

    fix

--- src/cb/codebreaker-UI.spec.js
+++ src/cb/codebreaker-UI.spec.js
@@ -2,13 +2,12 @@ import fs from "fs";
 
 describe("codebreaker UI", () => {
 
-    beforeAll(() => {
-        document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
-        require("../cb/presenter");
-    })
+  beforeAll(() => {
+      document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+      require("../cb/presenter");
+  })
 
   it("Saludo", () => {
- 
     expect(document.querySelector("title").innerHTML ).toEqual('¡Bienvenidos al Codebreaker!')
     expect(document.querySelector("h1").innerHTML ).toEqual('¡Bienvenidos al Codebreaker!')
   });
@@ -18,16 +17,14 @@ describe("codebreaker UI", () => {
     expect(resultado() ).toEqual('Ganó')
   })
  
-    function codigo(nro) {
-        document.querySelector("#codigo").value = nro
-    }
-    function adivinar(nro) {
-        document.querySelector("#numero").value = nro;
-        document.querySelector("input[type=submit]").click();
-    }
-    function resultado() {
-        return document.querySelector("#resultado").innerHTML
-    }
-
-
+  function codigo(nro) {
+      document.querySelector("#codigo").value = nro
+  }
+  function adivinar(nro) {
+      document.querySelector("#numero").value = nro;
+      document.querySelector("input[type=submit]").click();
+  }
+  function resultado() {
+      return document.querySelector("#resultado").innerHTML
+  }
 });

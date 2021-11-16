0525eee2ea32dff1f02a230584c47d3ca81b911c
Juan Gabardini <jgabardini@computer.org>
2021-11-16 14:16:02 -0300

    Co-authored-by: Alanroko99 <Alanroko99@users.noreply.github.com> Co-authored-by: IsabelG108 <IsabelG108@users.noreply.github.com> Co-authored-by: Normi1224 <Normi1224@users.noreply.github.com>

---
+++ src/cb/codebreaker-UI.spec.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+
+describe("codebreaker UI", () => {
+
+    beforeAll(() => {
+        document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+        require("../cb/presenter");
+    })
+
+  it("Saludo", () => {
+ 
+    expect(document.querySelector("title").innerHTML ).toEqual('¡Bienvenidos al Codebreaker!')
+    expect(document.querySelector("h1").innerHTML ).toEqual('¡Bienvenidos al Codebreaker!')
+  });
+  it('Ganó', () => {
+    codigo('3158')
+    adivinar('3158')
+    expect(resultado() ).toEqual('Ganó')
+  })
+ 
+    function codigo(nro) {
+        document.querySelector("#codigo").value = nro
+    }
+    function adivinar(nro) {
+        document.querySelector("#numero").value = nro;
+        document.querySelector("input[type=submit]").click();
+    }
+    function resultado() {
+        return document.querySelector("#resultado").innerHTML
+    }
+
+
+});
